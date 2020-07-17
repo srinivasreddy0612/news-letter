ccdfa12fdec3b9a1a92e22ee28367425bab3e64c
srinivas <srinivasreddy12ch@gmail.com>
2020-07-17 13:23:26 +0530

    place holder change

---
+++ app.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const bodyparser = require("body-parser");
+const request =require("request");
+const https = require("https")
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyparser.urlencoded({extended:true}));
+
+app.get("/",function(req, res){
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/",function(req,res){
+
+
+const firstname=req.body.fname;
+const lastname=req.body.lname;
+const email=req.body.ename;
+
+ const data={
+   members:[
+     {
+    email_address:email,
+    status: "subscribed",
+    merge_fields:{
+      FNAME:firstname,
+      LNAME:lastname
+
+    }
+     }
+   ]
+ };
+
+const jsonData = JSON.stringify(data) ;
+
+
+const url = "https://us10.api.mailchimp.com/3.0/lists/d50520316f" ;
+const options ={
+  method:"POST",
+  auth:"srinivas1:6f7620a033f5c77129a703a55e179910-us10"
+}
+
+  const request =https.request(url, options, function(response){
+
+  if(response.statusCode==200){
+    res.sendFile(__dirname + "/sucess.html");
+  }else {
+
+  res.sendFile(__dirname + "/failure.html");
+}
+
+
+response.on("data",function(data){
+  console.log(JSON.parse(data));
+})
+})
+request.write(jsonData);
+request.end();
+});
+app.post("/failure", function(req, res){
+  res.redirect("/")
+})
+
+
+app.listen(process.env.PORT || 4000 , function(){
+  console.log("server is running on port 3000");
+});
+
+
+
+// api key
+// ab27a4d4cc549f7048a5564dad003fe3-us10
+
+// audience id
+// d50520316f
